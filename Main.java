1e9073f368eca7979e9d037ab4ce7fae92e9c412
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-10-20 16:45:22 +0300

    dişardan alınan bir servisi sisteme entegre etmek Adapter kullanımı

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 package nLayeredDemo;
 
+import nLayeredDemo.JLogger.JLoggerManager;
 import nLayeredDemo.business.abstracts.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
 import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
 import nLayeredDemo.entities.concretes.Product;
 
@@ -10,8 +12,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ProductService productService =new ProductManager(new HibernateProductDao());
+        ProductService productService =new ProductManager(new HibernateProductDao(),new JLoggerManagerAdapter());
+        JLoggerManager jLoggerManager=new JLoggerManager();
         Product product =new Product(2,2,"Elma",12,50);
+
         productService.add(product);
 
     }
--- business/concretes/ProductManager.java
+++ business/concretes/ProductManager.java
@@ -2,16 +2,20 @@ package nLayeredDemo.business.concretes;
 
 import java.util.List;
 import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.core.LoggerService;
 import nLayeredDemo.dataAccess.abstracts.ProductDao;
 import nLayeredDemo.entities.concretes.Product;
 
 public class ProductManager implements ProductService {
 
     private ProductDao productDao;
+    private LoggerService loggerService;
 
-    public ProductManager(ProductDao productDao) {
+    public ProductManager(ProductDao productDao,LoggerService loggerService) {
         this.productDao = productDao;
+        this.loggerService=loggerService;
     }
+   
     @Override
     public void add(Product product) {
         if(product.getCategoryId()==1){
@@ -19,6 +23,8 @@ public class ProductManager implements ProductService {
             return;
         }
         this.productDao.add(product);
+        this.loggerService.logToSystem("ürün eklendi "+product.getName() );
+        
     }
 
     @Override
